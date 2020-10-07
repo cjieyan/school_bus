f0ce5a22e25c69d9c9675b0dec7bd59a7f8c2000
yancjie <yancjie@gmail.com>
2020-10-08 00:41:20 +0800

    授权认证

--- go-admin/router/xcx.go
+++ go-admin/router/xcx.go
@@ -9,13 +9,13 @@ import (
 // 无需认证的路由代码
 func registerXcxRouter(v1 *gin.RouterGroup) {
 	v1.POST("/login", xcx.Api{}.Login)
+	v1.POST("/record", xcx.Api{}.Record)
+	v1.POST("/faceinfo", xcx.Api{}.StudentFaceInfo)
 
 	//需要校验header的token
 	authGroup := v1.Group("/auth")
 	authGroup.Use(handler.XcxCheckToken)
 	{
 		authGroup.GET("/info", xcx.Api{}.Info)
-		authGroup.POST("/record", xcx.Api{}.Record)
-		authGroup.POST("/faceinfo", xcx.Api{}.StudentFaceInfo)
 	}
 }
