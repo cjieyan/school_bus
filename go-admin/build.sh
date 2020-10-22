88c0dd5104c8e2369012bfb1559395c0b82d9746
yancjie <310997792@qq.com>
2020-10-22 17:10:14 +0800

    重启进程

--- go-admin/build.sh
+++ go-admin/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp -r config/settings.yml.prod config/settings.yml && \
 go build -a -o go-admin . && \
+ps -ef |grep go-admin |grep -v grep|awk '{print $2}' |xargs -i kill -9 {} && \
 nohup  ./go-admin &
